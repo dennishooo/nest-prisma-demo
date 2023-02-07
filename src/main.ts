62b34eded2de8577efa0c57faa2f6ea9aa38181e
dennishooo <hkhdennis@gmail.com>
2023-02-07 10:45:52 +0800

    add whitelist and parseInt for params

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,8 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.useGlobalPipes(new ValidationPipe());
+  // non listed items in dto will be removed when whitelist is set to true
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
 
   const config = new DocumentBuilder()
     .setTitle('Median')
